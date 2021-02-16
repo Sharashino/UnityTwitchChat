5748928b5e3bbe517e74e6551efb102f01c0832d
Sharashino <sharashino@icloud.com>
2021-02-16 16:12:29 +0100

    Update GitIgnore and Create Simple UI

---
+++ Assets/MessageManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class MessageManager : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField messageInputField;
+    [SerializeField] private string nickname;
+
+}
---
+++ Assets/Scripts/Managers/ChatGridManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace TwitchChat.Managers
+{
+    public class ChatGridManager : MonoBehaviour
+    {
+        [SerializeField] private Transform chatWindow;
+        [SerializeField] private Transform chatMessageBox;
+        [SerializeField] private GameObject chatMessageBoxObj;
+
+        void Start()
+        {
+            //SpawnMessageBoxes();
+        }
+
+        private void SpawnMessageBoxes()
+        {
+            RectTransform chatWindowRect = chatWindow.GetComponent<RectTransform>();
+            RectTransform chatMessageBoxRect = chatMessageBox.GetComponent<RectTransform>();
+
+            int messageBoxesNum = (int)(chatWindowRect.rect.height / chatMessageBoxRect.rect.height);
+            Debug.Log(chatWindowRect.rect.height + " / " + chatMessageBoxRect.rect.height);
+
+
+            for (int i = 0; i <= messageBoxesNum; i++)
+            {
+                GameObject newChatBox = Instantiate(chatMessageBoxObj);
+                newChatBox.transform.parent = chatWindow;
+            }
+        }
+
+        public void SpawnMessageBox(string username, string message)
+        {
+            //TODO spawn message box with a given nick and message
+        }
+    }
+}
+
